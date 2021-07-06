372bd9ac34342ac95f691dca2263cd49c7987efd
Martin Orszulik <orszulik.m@outlook.com>
2021-07-06 22:53:56 +0200

    Update local variables

--- src/ruzova_app/_supabase.ts
+++ src/ruzova_app/_supabase.ts
@@ -1,8 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabase_url = "https://kwosefrhynbuphawdgtq.supabase.co";
-const supabase_anon = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyNTE1NzE0MiwiZXhwIjoxOTQwNzMzMTQyfQ.Zu0hC4JGG47TTw5sXbCLaOfKdmRxhRosiOMAkWP_Ugc";
-
-const supabase = createClient(supabase_url, supabase_anon);
+const supabase = createClient(process?.env?.SUPABASE_URL, process?.env?.SUPABASE_ANON_KEY);
 
 export default supabase;
