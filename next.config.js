8530e7c11a785729c0ed00b494bc747c9ee19e92
Martin Orszulik <orszulik.m@outlook.com>
2021-07-06 23:18:15 +0200

    Update local variables, into const

--- next.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  env: {
-    NEXT_PUBLIC_SUPABASE_URL: "https://kwosefrhynbuphawdgtq.supabase.co",
-    NEXT_PUBLIC_SUPABASE_ANON_KEY:
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyNTE1NzE0MiwiZXhwIjoxOTQwNzMzMTQyfQ.Zu0hC4JGG47TTw5sXbCLaOfKdmRxhRosiOMAkWP_Ugc",
-  },
-};
--- src/ruzova_app/_supabase.ts
+++ src/ruzova_app/_supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
+const supabase = createClient("https://kwosefrhynbuphawdgtq.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyNTE1NzE0MiwiZXhwIjoxOTQwNzMzMTQyfQ.Zu0hC4JGG47TTw5sXbCLaOfKdmRxhRosiOMAkWP_Ugc");
 
 
 export default supabase;
